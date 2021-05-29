0cb336f61538b413c705c21ed995a67709716bbc
Peter Nagy <peter.nagy@perit.hu>
2021-05-29 16:34:40 +0200

    run.bat

--- orchestrator/run.bat
+++ orchestrator/run.bat
@@ -8,8 +8,8 @@ if exist "%launcher%" (
 
 echo.@echo off>>"%launcher%"
 echo.ConsoleSize 200 20 200 9999>>"%launcher%"
-echo.cd build\install\webservice\bin>>"%launcher%"
+echo.cd build\install\orchestrator\bin>>"%launcher%"
 rem echo.set WEBSERVICE_OPTS=-Dspring.profiles.active=default,dev>>"%launcher%"
-echo.call webservice.bat>>"%launcher%"
+echo.call orchestrator.bat>>"%launcher%"
 
 start cmd /c "%launcher%"
